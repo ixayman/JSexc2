c8429a4bcca233ca65c041ac58340b054b3e5e68
ayman khalel <evoix.a@gmail.com>
2016-07-31 12:37:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var person={
+	name: "ayman",
+	eyeColor: "green",
+	height: 170
+}
+
+person.eyeColor= "black";
+alert(person.eyeColor);
+
+person.location= "israel?, haifa?";
+person.languages= "english?, hebrew?, arabic?";
+alert(typeof("hebrew?"));
+
